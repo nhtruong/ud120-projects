0d2237fe8ade38f55dbc451e5d33bdff24376b3d
Theo Truong <chocobos8687@gmail.com>
2015-12-16 15:53:49 -0600

    DONE!

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -9,18 +9,6 @@ from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 from tester import test_classifier
 
-from sklearn.cross_validation import train_test_split
-from sklearn.feature_selection import SelectKBest as SKB
-from sklearn.preprocessing import MinMaxScaler as MMS
-from sklearn.decomposition import PCA
-
-from sklearn.tree import DecisionTreeClassifier as DTC
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier as KNC
-
-from sklearn.grid_search import GridSearchCV
-from sklearn.pipeline import Pipeline, FeatureUnion
-
 #----------------------------------------------------------
 #   Load the dictionary containing the dataset
 #----------------------------------------------------------
@@ -47,6 +35,7 @@ for f in count_na:
 #----------------------------------------------------------
 #   Create new features
 #----------------------------------------------------------
+
 for k in data_dict:
     val = data_dict[k]
     data_dict[k]['grand_total'] = val['total_payments'] + val['total_stock_value']
@@ -54,34 +43,43 @@ for k in data_dict:
         1. * val['from_poi_to_this_person'] / val['to_messages'] if val['to_messages'] > 0 else 0
     data_dict[k]['to_poi_ratio'] = \
         1. * + val['from_this_person_to_poi'] / val['from_messages'] if val['from_messages'] > 0 else 0
-# Store to my_dataset for easy export below.
-my_dataset = data_dict
 
 #----------------------------------------------------------
 #   Function That Builds a Classification Model
 #----------------------------------------------------------
 
-def build_model(selected_features, classifier, parameters, use_kbest = False, use_pca = False):
+from sklearn.feature_selection import SelectKBest
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+
+def build_model(selected_features, classifier, parameters={}, scoring = 'f1', use_kbest = False, use_pca = False):
     # Extract features and labels from dataset
-    data = featureFormat(my_dataset, selected_features, sort_keys = True)
+    data = featureFormat(data_dict, selected_features, sort_keys = True)
     labels, features = targetFeatureSplit(data)
 
     # Add pipeline prefix for parameters
     parameters = {'classifier__'+k:parameters[k] for k in parameters}
 
-    transformer = [('scaler',MMS())]
+    transformer = []
 
     if use_kbest:
-        transformer += [('selector',SKB())]
-        parameters['transformer__selector__k'] = range(1,len(selected_features)-1)
+        transformer += [('kbest',SelectKBest())]
+        parameters['transformer__kbest__k'] = range(1,len(selected_features)-1)
 
     if use_pca:
-        transformer += [("pca",PCA())]
+        transformer += [('scaler',MinMaxScaler()),("pca",PCA())]
         parameters['transformer__pca__n_components'] = range(1,len(selected_features))
 
-    transformer = FeatureUnion(transformer)
-    pipeline = Pipeline([("transformer",transformer),("classifier",classifier)])
-    grid = GridSearchCV(pipeline, parameters, scoring='f1', n_jobs=-1, cv=10)
+    if len(transformer) <> 0:
+        transformer = FeatureUnion(transformer)
+        pipeline = Pipeline([("transformer",transformer),("classifier",classifier)])
+    else:
+        pipeline = Pipeline([("classifier",classifier)])
+
+    grid = GridSearchCV(pipeline, parameters, scoring=scoring, n_jobs=-1, cv=10)
     grid.fit(features, labels)
     clf = grid.best_estimator_
 
@@ -89,8 +87,8 @@ def build_model(selected_features, classifier, parameters, use_kbest = False, us
     test_classifier(clf,data_dict,selected_features)
     print '-----------------------------------------------\n'
 
-    print 'Best f1-score:', grid.best_score_
-    print 'Best parameters:', grid.best_params_, '\n'
+    print 'Best '+scoring+'-score:', grid.best_score_
+    print 'Best Parameters:', grid.best_params_, '\n'
 
     return clf
 
@@ -98,53 +96,72 @@ def build_model(selected_features, classifier, parameters, use_kbest = False, us
 #   Compare the Performance of 3 Different Algorithms
 #----------------------------------------------------------
 
-features_list = ['poi',
-                 'total_stock_value', 'total_payments', 'grand_total',
-                 'from_this_person_to_poi','from_poi_to_this_person','shared_receipt_with_poi',
-                 'from_poi_ratio','to_poi_ratio']
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier as DTC
+from sklearn.neighbors import KNeighborsClassifier as KNC
 
-build_model(features_list, SVC(), {})
-build_model(features_list, DTC(), {})
-build_model(features_list, KNC(), {})
+# select all numerical features
+all_features = ['poi']+[k for k in data_dict[data_dict.keys()[0]] if k not in ['poi','email_address']]
+
+build_model(all_features, SVC())
+build_model(all_features, DTC())
+build_model(all_features, KNC())
+
+# Decicion Tree takes the cake here!!!
 
 #----------------------------------------------------------
-#   Decide which algorithm to use
+#   Select Features
+#----------------------------------------------------------
+
+data = featureFormat(data_dict, all_features, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+
+# Calculate Feature Scores:
+skb = SelectKBest(k='all')
+skb.fit(features,labels)
+scores = zip(skb.scores_, all_features[1:])
+print "Feature Scores:"
+for x in sorted(scores, key=lambda x: -x[0]):
+    print x
+
+
+# Pick the top features that are not dominated by NA values
+features_list = ['poi',
+                 'to_poi_ratio', 'shared_receipt_with_poi', 'from_poi_to_this_person',
+                 'from_poi_ratio','from_this_person_to_poi','to_messages']
+
+# Use SelectKBest in GridSearchCV to find the best number of features
+clf1 = build_model(features_list, DTC(), {}, use_kbest = True)
+
+# Best number of features is 2
+features_list = ['poi',
+                 'to_poi_ratio', 'shared_receipt_with_poi']
+
+
+#----------------------------------------------------------
+#   Tuning Parameters for the Decision Tree
 #----------------------------------------------------------
 
 
-
-features_list = ['poi',
-                 'from_this_person_to_poi','from_poi_to_this_person','shared_receipt_with_poi',
-                 'from_poi_ratio','to_poi_ratio']
-
-features_list = ['poi',
-                 'total_stock_value', 'total_payments', 'grand_total']
-
-clf = build_model(features_list, DTC(),
+# With PCA
+clf2 = build_model(features_list, DTC(max_features=None),
                   {'criterion':['gini','entropy'],
-                   'max_features':[None,'sqrt','log2'],
                    'max_depth':[1,2,3],
                    'min_samples_split':[2,3,4]},
-                  use_kbest = False,
                   use_pca= True)
 
-
-features_list = ['poi',
-                 'shared_receipt_with_poi','from_poi_ratio','to_poi_ratio']
-
-clf = build_model(features_list, DTC(),
+# Without PCA
+clf3 = build_model(features_list, DTC(max_features=None),
                   {'criterion':['gini','entropy'],
-                   'max_features':[None],
                    'max_depth':[1,2,3],
-                   'min_samples_split':[2,3,4]},
-                  use_kbest = False,
-                  use_pca= True)
+                   'min_samples_split':[2,3,4]})
 
 
 
 #----------------------------------------------------------
-### Dump Classifier and Data
+#    Dump Classifier and Data
 #----------------------------------------------------------
-
-
+clf = clf2
+my_dataset = data_dict
 dump_classifier_and_data(clf, my_dataset, features_list)
